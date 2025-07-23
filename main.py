e239fc3478652017036cdfab50bdbbe3dd7713ae
Pesti411 <pesti@posteo.de>
2025-07-23 18:19:27 +0200

    Update main.py
    complete rewrite gpt is at fault

--- main.py
+++ main.py
@@ -4,22 +4,13 @@ import feedparser
 import re
 import asyncio
 import html
-
-intents = discord.Intents.default()
-intents.message_content = True  # Damit dein Bot Nachrichten-Inhalte lesen kann
-
-client = discord.Client(intents=intents)
+import threading
+import http.server
+import socketserver
 
 TOKEN = os.getenv("DISCORD_TOKEN")
-BLACKLIST_CHANNELS = ["discord-vorschläge", "umfragen", "roleplay", "vertonungsplan", "news"]
-
-@client.event
-async def on_message(message):
-    if message.author.bot:
-        return
-    if message.channel.name in BLACKLIST_CHANNELS:
-        return
 FEED_URL = "https://q8reci.podcaster.de/scp-deutsch.rss"
+BLACKLIST_CHANNELS = ["discord-vorschläge", "umfragen", "roleplay", "vertonungsplan", "news"]
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -60,25 +51,19 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.author.bot:
-        return
-    if message.channel.name in BLACKLIST_CHANNELS:
+    if message.author.bot or message.channel.name in BLACKLIST_CHANNELS:
         return
 
     msg = message.content.upper()
 
-    for code, link in scp_links.items():
+    for code, data in scp_links.items():
         code_upper = code.upper()
         pattern = r'(?<![\w-])' + re.escape(code_upper) + r'(?![\w-])'
         if re.search(pattern, msg, re.IGNORECASE):
             await message.channel.send(
-                f"🔎 Gefunden: **{code.upper()}**\n🎧 **[Hier anhören]({link})**"
+                f"🔎 Gefunden: **{data['title']}**\n🎧 **[Hier anhören]({data['link']})**"
             )
             break
-
-import threading
-import http.server
-import socketserver
 
 def keep_alive():
     handler = http.server.SimpleHTTPRequestHandler
